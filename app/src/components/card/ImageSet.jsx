6f2ea1c667e492d445a62b231dba67a86aaff2c8
elisa-desmetz <elisa.desmetz@gmail.com>
2024-09-13 01:58:04 +0200

    square fusionDex img bulbasaur

--- app/src/components/card/ImageSet.jsx
+++ app/src/components/card/ImageSet.jsx
@@ -1,19 +1,19 @@
-export default function ImageSet({isShiny, imgPath}){
+export default function ImageSet({ isShiny, imgPath }) {
     let path
-    if (isShiny){
-        path = imgPath+'-s.webp'
+    if (isShiny) {
+        path = imgPath + '-s.webp'
     }
     else {
-        path = imgPath+'.webp'
+        path = imgPath + '.webp'
     }
-    return(
+    return (
         <>
-        <div className="minisprite">
-            <img src={'/images/pokemon/minisprite/'+path}/>
-        </div>
-        <div className="sprite pokeball_back">
-            <img src={'/images/pokemon/sprite/'+path}/>
-        </div>
+            <div className="minisprite">
+                <img src={'/images/pokemon/minisprite/' + path} loading="lazy" />
+            </div>
+            <div className="sprite pokeball_back">
+                <img src={'/images/pokemon/sprite/' + path} loading="lazy" />
+            </div>
         </>
     )
 }
