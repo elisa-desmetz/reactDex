f00e2cdc003f13a175bf4a9b290bb667534e3e6f
elisa-desmetz <elisa.desmetz@gmail.com>
2024-09-13 02:41:49 +0200

    img onError fallback path

--- app/src/components/card/ImageSet.jsx
+++ app/src/components/card/ImageSet.jsx
@@ -6,13 +6,27 @@ export default function ImageSet({ isShiny, imgPath }) {
     else {
         path = imgPath + '.webp'
     }
+    const fallback = imgPath + '.webp'
+
     return (
         <>
             <div className="minisprite">
-                <img src={'/images/pokemon/minisprite/' + path} loading="lazy" />
+                <img
+                    onError={(e) => {
+                        e.target.src = '/images/pokemon/minisprite/' + fallback
+                    }}
+                    src={'/images/pokemon/minisprite/' + path}
+                    loading="lazy"
+                />
             </div>
             <div className="sprite pokeball_back">
-                <img src={'/images/pokemon/sprite/' + path} loading="lazy" />
+                <img
+                    onError={(e) => {
+                        e.target.src = '/images/pokemon/sprite/' + fallback
+                    }}
+                    src={'/images/pokemon/sprite/' + path}
+                    loading="lazy"
+                />
             </div>
         </>
     )
